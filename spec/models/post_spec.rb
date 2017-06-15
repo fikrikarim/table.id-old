a0be613cc6174d8d3b5f371bb23c801137975a7c
Fikri <fkfikrikarim@gmail.com>
2017-06-15 23:16:19 +0700

    Post require title and text

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Post, type: :model do
     expect(build(:post)).to be_valid
   end
 
+  it "is invalid without title" do
+      post = build(:post, title: nil)
+      post.valid?
+      expect(post.errors[:title]).to include("must exist")
+  end
+
+  it "is invalid without text" do
+      post = build(:post, text: nil)
+      post.valid?
+      expect(post.errors[:text]).to include("must exist")
+  end
+
   # it "is invalid without user" do
   #   post = build(:post, user: nil)
   #   post.valid?
