22a4d50c8ab094b825524cc81ab80cc938993655
Fikri <fkfikrikarim@gmail.com>
2017-06-15 23:18:57 +0700

    Add User association to Post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :post do
-    # association :user
+    association :user
     title "[OSN Story] Kebanggaan Orang Tua"
     text "Jadi ini cerita bagaimana saya bisa membahagiakan orang tua saya."
   end
