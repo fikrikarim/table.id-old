195bc5750a8a1341b3ef7311bd074be471786116
Fikri <fkfikrikarim@gmail.com>
2017-06-16 15:41:12 +0700

    Add username to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ApplicationRecord
   has_many :post
   has_many :comment
 
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+  } # etc.
+
+  validate :validate_username
+  attr_accessor :login
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_h).first
+    end
+  end
+
 end
