be0cadec72ef33feae161d89e494e7e9df1a519c
Fikri <fkfikrikarim@gmail.com>
2017-06-17 15:49:00 +0700

    Add pending test to home_spec feature test

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'home' do
+  pending "Display max 20 post"
+  pending "Display the title correctly, and create the link"
+  pending "Display the index correctly, and create the link"
+  pending "Display the about time ago correctly"
+  pending "Display the submitter correctly, and create the link"
+  pending "Display the amounts of comments and the link"
+  pending "Display the amounts of vote the post has"
+  pending "Display the upvote button, and the link"
+  pending "Display the downvote button, and the link"
+
+end
