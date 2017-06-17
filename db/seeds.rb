f6837e23d2411c2df66753f9d1cdb7708c311f55
Fikri <fkfikrikarim@gmail.com>
2017-06-17 12:46:57 +0700

    Update seeds.rb to create more post

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,65 @@ user2 = User.create! username: "jh",
                     password: "jamal321",
                     password_confirmation: "jamal321"
 
+user3 = User.create! username: "annisa",
+                    email: "annisa@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+
+user4 = User.create! username: "angga",
+                    email: "angga@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+
+user5 = User.create! username: "maria",
+                    email: "maria@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+
+user6 = User.create! username: "kris",
+                    email: "kris@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+
+user7 = User.create! username: "david",
+                    email: "david@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+
+user8 = User.create! username: "ica21",
+                    email: "ica21@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+
 post1 = Post.create! title: "[OSN Story] Perjuangan Keras Yang Membanggakan Kedua Orang Tua",
                     text: "Ini ceritaku, mana ceritamu",
+                    user: user1
+
+post2 = Post.create! title: "[OSN Story] Jalan-jalan dan dibayar",
+                    text: "Ini ceritaku, mana ceritamu",
                     user: user2
+
+post3 = Post.create! title: "Planet baru yang sangat mirip bumi ditemukan",
+                    text: "Ini ceritaku, mana ceritamu",
+                    user: user3
+
+post4 = Post.create! title: "[OSN Story] Mencari Ridho Tuhan",
+                    text: "Ini ceritaku, mana ceritamu",
+                    user: user4
+
+post5 = Post.create! title: "[OSN Story] Kenangan yang paling indah disaat SMA",
+                    text: "Ini ceritaku, mana ceritamu",
+                    user: user5
+
+post6 = Post.create! title: "Samsung galaxy S14 diluncurkan",
+                    text: "Ini ceritaku, mana ceritamu",
+                    user: user6
+
+post7 = Post.create! title: "[OSN Story] Provinsi aja ngga lolos",
+                    text: "Ini ceritaku, mana ceritamu",
+                    user: user7
+
+post8 = Post.create! title: "[OSN Story] Mencari Ridho Tuhan",
+                    text: "Ini ceritaku, mana ceritamu",
+                    user: user8
 
