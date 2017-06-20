d6cce369b9987f77109fdf0a0148fe54dab719d8
Fikri <fkfikrikarim@gmail.com>
2017-06-20 22:31:31 +0700

    Change users factories to not use faker again because sometimes it won't work because duplicate

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,16 @@
 FactoryGirl.define do
+  sequence :username do |n|
+    "user#{n}@table.com"
+  end
+  sequence :email do |n|
+    "user#{n}@table.com"
+  end
 
   # If email to confirmable
   # factory :unconfirmed_user, :class => 'User' do
   factory :user, :class => 'User' do
-    username { Faker::Pokemon.name }
-    email { Faker::Internet.email }
+    username
+    email
     password '12345678'
     password_confirmation '12345678'
 
