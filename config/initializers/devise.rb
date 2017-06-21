36129ba249ce80c3b6e697e2abe4ca13c73a45ec
Fikri <fkfikrikarim@gmail.com>
2017-06-21 11:39:43 +0700

    Edit mailer sender for devise mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'no-reply@table.id'
 
   # Speed up your unit tests
   # https://github.com/plataformatec/devise/wiki/Speed-up-your-unit-tests
