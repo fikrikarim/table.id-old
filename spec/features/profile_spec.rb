c4c41027e881631600da0d099da30b7abc2cd68c
Fikri <fkfikrikarim@gmail.com>
2017-06-29 17:19:36 +0700

    Add test and implementation to only show 'Edit profile' button to the same user

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -10,4 +10,19 @@ feature 'Profile' do
     expect(page).to have_content(user.username)
     expect(page).to have_content(user.full_name)
   end
+
+  scenario "if the visiting the user isnt the logged one, doesn't show 'Edit Profile' button" do
+    user1 = create(:user)
+    user2 = create(:user)
+    login(user2)
+    visit user_profile_path(user1.username)
+    expect(page).not_to have_content('Edit profile')
+  end
+
+  scenario "if the visiting the user is the logged one show 'Edit Profile' button" do
+    user1 = create(:user)
+    login(user1)
+    visit user_profile_path(user1.username)
+    expect(page).to have_content('Edit profile')
+  end
 end
