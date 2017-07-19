f342ee70e0276b8ccd916bb6c5a84cda6e588e7f
Fikri <fkfikrikarim@gmail.com>
2017-07-19 15:11:17 +0700

    Set default value for 'Full name' and 'Bio' in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by username: params[:username]
+
+    @user.full_name ||= 'Nama Lengkap'
+    @user.bio ||= 'Bio'
   end
 
   def edit
